9a80c7693beec465f379eafa7e566684626da2f2
alecxander567 <alecxanderespaldon21@gmail.com>
2026-04-14 10:26:57 +0800

    fix: rename useItemcount to useItemCount case sensitivity

--- src/hooks/useItemCount.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { ItemCountContext } from "../components/ItemCountContext";
-
-export function useItemCount() {
-  return useContext(ItemCountContext);
-}
